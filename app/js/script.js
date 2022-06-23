f84d97c85d1da9ce0d4ff3b9ae6f551888ce2065
RmNLcN06 <romain.luciano@orange.fr>
2022-06-23 23:16:29 +0200

    Responsive for Categories done

--- app/js/script.js
+++ app/js/script.js
@@ -185,7 +185,7 @@
 
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "180px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
